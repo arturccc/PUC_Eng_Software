f5cc859d51f9352216586b10fea990cd68341237
artur.coelho <artur.coelho@pdcase.com.br>
2024-03-19 01:43:23 -0300

    feat[SQLWeb]: Alert msg e regras de cadastro

---
+++ 1_Semestre/DIW/SQLWeb/readme.md
@@ -0,0 +1,4 @@
+Atividade de criação de 3 paginas, junto a um teste de manipulação do localStorage do JavaScript para criação de um db "SQLite" no proprio navegador
+################################################################################################
+INICIADO EM 18/02/24
+FINALIZADO EM
--- 1_Semestre/DIW/SQLWeb/scripts/app.js
+++ 1_Semestre/DIW/SQLWeb/scripts/app.js
@@ -8,12 +8,47 @@ function cadastro(){
     let confirmpassinput = document.getElementById('confirmpass');
     let confirmpass = confirmpassinput.value
 
-    let erro = document.getElementById('erro');
 
-    if(pass === confirmpass){
-        window.location.href = "confirm.html"
-    }else{
-        erro.innerHTML = "Senhas não iguais"
+    if(!user){
+        gerarAlerta("Preencha o campo de usuario!")
     }
 
+    if(!email){
+        gerarAlerta("Preencha o campo de email!") 
+    }
+
+    if(pass && confirmpass){
+        if(pass === confirmpass){
+            let numV = /[a-zA-Z]/.test(pass);
+            let letraV = /\d/.test(pass);
+            if(pass.length < 5){
+                gerarAlerta("Digite uma senha com 5 ou mais caracteris")
+            }else if(!numV || !letraV){
+                gerarAlerta("Sua senha deve possuir numeros e letras")
+            }else{
+                window.location.href = "confirm.html"
+            }
+            
+        }else{
+            gerarAlerta("Senhas não iguais!");
+        }
+    }else if(!confirmpass && pass){
+        gerarAlerta("Por Favor confirme sua senha!")
+    }else if(!pass && confirmpass || !pass && !confirmpass){
+        gerarAlerta("Por Favor digite uma senha!")
+    }
+     
+}
+
+let alertaBox = document.getElementById('alertaBox');
+
+function gerarAlerta(msg){
+    let  alerta = document.createElement('div');
+    alerta.classList.add('alerta');
+    alerta.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>' + msg;
+    alertaBox.appendChild(alerta)
+
+    setTimeout(()=>{
+        alerta.remove();
+    }, 4000)
 }
