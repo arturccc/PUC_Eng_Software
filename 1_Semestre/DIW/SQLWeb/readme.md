a4727619534920562fc6312934ade9eb03da3a2c
artur.coelho <artur.coelho@pdcase.com.br>
2024-03-19 11:16:49 -0300

    feat[SQLWeb]: finalização

--- 1_Semestre/DIW/SQLWeb/readme.md
+++ 1_Semestre/DIW/SQLWeb/readme.md
@@ -1,4 +1,4 @@
 Atividade de criação de 3 paginas, junto a um teste de manipulação do localStorage do JavaScript para criação de um db "SQLite" no proprio navegador
 ################################################################################################
-INICIADO EM 18/02/24
-FINALIZADO EM
+INICIADO EM 18/03/24
+FINALIZADO EM 19/03/24
--- 1_Semestre/DIW/SQLWeb/scripts/app.js
+++ 1_Semestre/DIW/SQLWeb/scripts/app.js
@@ -1,13 +1,23 @@
+let user;
+let email;
+let pass;
+
+function verificacao(){
+    let user = localStorage.getItem("user");
+    if(user){
+        window.location.href = "home.html"
+    }
+}
+
 function cadastro(){
     let userinput = document.getElementById('user');
-    let user = userinput.value;
+    user = userinput.value;
     let emailinput = document.getElementById('email');
-    let email = emailinput.value
+    email = emailinput.value
     let passinput = document.getElementById('pass');
-    let pass = passinput.value;
+    pass = passinput.value;
     let confirmpassinput = document.getElementById('confirmpass');
     let confirmpass = confirmpassinput.value
-
 
     if(!user){
         gerarAlerta("Preencha o campo de usuario!")
@@ -26,7 +36,16 @@ function cadastro(){
             }else if(!numV || !letraV){
                 gerarAlerta("Sua senha deve possuir numeros e letras")
             }else{
-                window.location.href = "confirm.html"
+                let userV = localStorage.getItem("user");
+                if(!userV){
+                    localStorage.setItem("user", user);
+                    localStorage.setItem("email", email);
+                    localStorage.setItem("pass", pass);
+                    window.location.href = "confirm.html"
+                }else{
+                    window.location.href = "confirm.html"
+                }
+                
             }
             
         }else{
@@ -52,3 +71,54 @@ function gerarAlerta(msg){
         alerta.remove();
     }, 4000)
 }
+
+let salvar = false
+
+function savetrue(){
+    salvar = true
+    save()
+}
+
+function savefalse(){
+    salvar = false
+    save()
+}
+
+function save(){
+    let user = localStorage.getItem("user");
+    let email = localStorage.getItem("email");
+    let pass = localStorage.getItem("pass");
+
+
+    if(!salvar){
+        localStorage.clear();
+        window.location.href = "index.html"
+    }else{
+        window.location.href = "home.html"
+    }
+}
+
+function home(){
+    let usr = document.getElementById('usr');
+    let eml = document.getElementById('eml');
+    let sen = document.getElementById('sen');
+
+    console.log(usr);
+    console.log(eml);
+    console.log(sen);
+
+    let user = localStorage.getItem("user");
+    let email = localStorage.getItem("email");
+    let pass = localStorage.getItem("pass");
+
+
+    usr.innerHTML = 'Usuario: ' + user
+    eml.innerHTML = 'Usuario: ' + email
+    sen.innerHTML = 'Usuario: ' + pass
+}
+
+function delet(){
+    localStorage.clear();
+    window.location.href = "index.html"
+}
+
